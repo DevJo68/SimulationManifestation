2b7a1f1436846b65c469c2249f4bb6b261fac0e2
DevJo68 <joelbtssio803@gmail.com>
2019-10-23 09:56:47 +0200

    Modification du vendredi 18 octobre :  - Ajout de la classe CelluleObjectif  - Ajout de la classe GroupeManifestant  - Ajout de la classe GroupePolicier  - Modification apporté au classes Policier,Groupe et Manifestant

---
+++ SystemeAvance/src/systemeavance/GroupeManifestant.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package systemeavance;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author e1603982
+ */
+public class GroupeManifestant extends Groupe {
+   public GroupeManifestant(){
+       super();
+   }
+   @Override
+   public void Deplacement(){
+       //On teste si il ya des policiers proche d'un membre
+       //si oui on fait reculer le groupe
+       //sinon deplacer vers lobjectif
+   }
+
+}
---
+++ SystemeAvance/src/systemeavance/GroupePoliciers.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package systemeavance;
+
+import java.util.ArrayList;
+
+/**
+ *le Groupe contient tous les policiers
+ * @author e1603982
+ */
+public class GroupePoliciers extends Groupe {
+   public GroupePoliciers(){
+       super();
+   }
+   @Override
+   public void Deplacement(){
+       //On teste si il ya des manifestant proche d'un membre
+       //si oui on fait charger le crs avec le X crs le plus proches
+       //sinon deplacer vers lobjectif a deplacer
+   }
+
+}
